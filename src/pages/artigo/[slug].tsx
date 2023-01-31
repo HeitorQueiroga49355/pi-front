78f6846dd831194e776dbde55a36e356ce4a9fcf
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2023-01-31 20:25:02 -0300

    Envio da página de artigo

---
+++ src/pages/artigo/[slug].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../components/organism/Footer'
+import Header from '../../components/organism/Header'
+import TemplateArticle from '../../components/templates/TemplateArticle'
+
+export default function Article() {
+  return (
+    <>
+      <Header />
+      <TemplateArticle />
+      <Footer />
+    </>
+  )
+}
